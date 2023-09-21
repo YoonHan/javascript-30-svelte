5485f0bb13d4f5f2acfb1ea12d37c66c64d133df
Yoon Han <yoontopia94@gmail.com>
2023-09-21 15:24:23 +0900

    feat: add background image to wrapper

---
+++ src/routes/javascript-drum-kit/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<main class="wrapper">
+	<section class=""></section>
+</main>
+
+<style>
+	.wrapper {
+		width: 100vw;
+		height: 100vh;
+		background: url('./images/background.jpg');
+	}
+</style>
