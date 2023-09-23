460baf7a19b3d150b4f08701cd6a94e6716ef272
Yoon Han <yoontopia94@gmail.com>
2023-09-23 23:48:55 +0900

    feat: implement js-css-clock

--- src/routes/js-and-css-clock/+page.svelte
+++ src/routes/js-and-css-clock/+page.svelte
@@ -1,4 +1,33 @@
 <script lang="ts">
+	import { onMount } from 'svelte'
+	import { SECOND } from './constants'
+
+	const secondCssVar = '--second'
+	const hourDegPerSecondCssVar = '--hourDegPerSecond'
+	const minuteDegPerSecondCssVar = '--minuteDegPerSecond'
+	const secondDegPerSecondCssVar = '--secondDegPerSecond'
+
+	onMount(() => {
+		const documentEl = document.documentElement
+		documentEl.style.setProperty(secondCssVar, '0')
+		documentEl.style.setProperty(hourDegPerSecondCssVar, 'calc(30deg / 3600)')
+		documentEl.style.setProperty(minuteDegPerSecondCssVar, 'calc(6deg / 60)')
+		documentEl.style.setProperty(secondDegPerSecondCssVar, 'calc(360deg / 60)')
+
+		const updateSecondIntervalId = setInterval(() => {
+			if (!documentEl) {
+				return
+			}
+
+			const currentSecond = documentEl.style.getPropertyValue(secondCssVar)
+			const nextSecond = `${Number(currentSecond) + 1}`
+			documentEl.style.setProperty(secondCssVar, nextSecond)
+		}, 1 * SECOND)
+
+		return () => {
+			clearInterval(updateSecondIntervalId)
+		}
+	})
 </script>
 
 <svelte:head>
@@ -9,7 +38,7 @@
 <div class="clock">
 	<div class="clock-face">
 		<div class="hand hour-hand"></div>
-		<div class="hand min-hand"></div>
+		<div class="hand minute-hand"></div>
 		<div class="hand second-hand"></div>
 	</div>
 </div>
@@ -56,9 +85,26 @@
 
 	.hand {
 		width: 50%;
-		height: 6px;
-		background: black;
 		position: absolute;
 		top: 50%;
+		transform-origin: 100%;
+	}
+
+	.hour-hand {
+		background: rgba(0, 0, 0, 0.9);
+		height: 6px;
+		transform: rotate(calc(var(--second) * var(--hourDegPerSecond)));
+	}
+
+	.minute-hand {
+		background: rgba(0, 0, 0, 0.8);
+		height: 5px;
+		transform: rotate(calc(var(--second) * var(--minuteDegPerSecond)));
+	}
+
+	.second-hand {
+		background: rgba(0, 0, 0, 0.7);
+		height: 4px;
+		transform: rotate(calc(var(--second) * var(--secondDegPerSecond)));
 	}
 </style>
